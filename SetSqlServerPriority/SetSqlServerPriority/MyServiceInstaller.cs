80b014ca06a5e00e4c66b56008c5fa9070155bc1
David Colvin <david.colvin@aumentumtech.com>
2023-06-02 11:09:01 -0400

    Add installer and logging

---
+++ SetSqlServerPriority/SetSqlServerPriority/MyServiceInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+[RunInstaller(true)]
+public class MyServiceInstaller : Installer
+{
+  public MyServiceInstaller()
+  {
+    var serviceProcessInstaller = new ServiceProcessInstaller();
+    var serviceInstaller = new ServiceInstaller();
+
+    // Configure the service process installer
+    serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+
+    // Configure the service installer
+    serviceInstaller.ServiceName = "SQLPriorityService";
+    serviceInstaller.DisplayName = "SQL Priority Service";
+    serviceInstaller.Description = "Sets Priority of SQL Server";
+    serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+    // Add the installers to the installer collection
+    Installers.Add(serviceProcessInstaller);
+    Installers.Add(serviceInstaller);
+  }
+}
--- SetSqlServerPriority/SetSqlServerPriority/SQLPriorityService.cs
+++ SetSqlServerPriority/SetSqlServerPriority/SQLPriorityService.cs
@@ -8,14 +8,17 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace SetSqlServerPriority
 {
   partial class SQLPriorityService : ServiceBase
   {
+    log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); //LogHelper.GetLogger();
     private System.Timers.Timer timer;
     
     public SQLPriorityService()
     {
+      log.Debug("SQLPriorityService starting...");
       InitializeComponent();
 
       timer = new System.Timers.Timer();
@@ -26,6 +29,7 @@ namespace SetSqlServerPriority
 
     protected override void OnStart(string[] args)
     {
+      log.Debug("OnStart");
       timer.Start();
       CheckAndSetPriority();
 
@@ -33,34 +37,43 @@ namespace SetSqlServerPriority
 
     protected override void OnStop()
     {
+      log.Debug("OnStop");
       timer.Stop();
     }
 
     // This will be called every minute
     public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
     {
+      log.Debug("OnTimer");
       CheckAndSetPriority();
     }
 
     private void CheckAndSetPriority()
     {
+      log.Debug("CheckAndSetPriority");
       // Replace "sqlservr" with the actual name of your SQL Server process
       var processes = Process.GetProcessesByName("sqlservr");
       foreach (var process in processes)
       {
+        log.Debug("found a process");
         // Make sure the process is running
         if (process != null)
         {
+          log.Debug("process is not null");
           try
           {
+            log.Debug(process.PriorityClass.ToString());
             // Set priority to high
             process.PriorityClass = ProcessPriorityClass.High;
+            log.Debug(process.ToString());
+            log.Debug(process.PriorityClass.ToString());
+            timer.Stop();
           }
           catch (Exception ex)
           {
             // Log error, if any
             // You might want to use a logging framework like log4net or NLog for this
-            Console.WriteLine(ex.ToString());
+            log.Error(ex.ToString());
           }
         }
       }
